f5fdc3f4e7b91705d6042672a495d6a9df5a36f0
SubOfTheDarkness <arduino.unopro@yandex.ru>
2026-05-14 11:41:18 +0300

    main

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+@app.route("/admin")
+def admin_panel():
+    return "<p>Это админ панель будет короче<p>"
