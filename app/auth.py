ba5ecc9a9e6e8b0f98b3dee84aed4b6e174ac809
Octotacle <volosenukdaniil9@gmail.com>
2026-05-19 19:31:08 +0300

    Починил html файлы 	modified:   app/auth.py 	modified:   app/cart.py 	modified:   app/catalogue.py 	modified:   app/templates/admin/admin.html 	modified:   app/templates/auth/login.html 	modified:   app/templates/cart/cart.html 	modified:   app/templates/cart/checkout.html 	modified:   app/templates/catalogue/catalog.html 	modified:   app/templates/catalogue/product.html 	modified:   instance/flaskr.sqlite

--- app/auth.py
+++ app/auth.py
@@ -41,7 +41,7 @@ def register():
                 if error is None:
                     session.clear()
                     session['user_id'] = user['id']
-                    return redirect(url_for('index'))
+                    return redirect(url_for('catalogue.catalogue'))
 
         flash(error)
 
@@ -66,7 +66,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('catalogue.catalogue'))
 
         flash(error)
 
@@ -86,7 +86,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('catalogue.catalogue'))
 
 def login_required(view):
     @functools.wraps(view)
