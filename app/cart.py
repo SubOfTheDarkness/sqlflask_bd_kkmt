5e99170df14bd041b03ec64f71a06f7c97c0a6fd
SubOfTheDarkness <arduino.unopro@yandex.ru>
2026-05-20 07:12:41 +0300

    Сделал изменение количества товара в корзине по книпкам 	modified:   app/cart.py 	modified:   app/templates/cart/cart.html 	modified:   app/templates/cart/checkout.html 	modified:   instance/flaskr.sqlite

--- app/cart.py
+++ app/cart.py
@@ -1,19 +1,34 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 from app.db import get_db
 
 bp = Blueprint('cart', __name__)
 
+@bp.route('/update_count', methods=['POST'])
+def update_count():
+    data = request.get_json()
+    val = data.get('value')
+    item_ids = data.get('item_id')
+    try:
+        db = get_db()
+        db.execute(
+            'UPDATE cart'
+            ' SET quantity=?'
+            ' WHERE id=?', (val,item_ids))
+        db.commit()
+        return jsonify({'status': 'success'})
+
+    except Exception as e:
+        return jsonify({'status': 'error'})
 
 @bp.route('/cart')
 def cart():
     """Страница корзины."""
     db = get_db()
     cart_items = db.execute(
-        'SELECT p.id, p.title, p.price, c.quantity, p.category, p.image'
+        'SELECT c.id AS item_id,c.product_id, p.title, p.price, c.quantity, p.category, p.image'
         ' FROM cart c'
         ' JOIN product p ON p.id = c.product_id'
     ).fetchall()
-
     total = sum(item['price'] * item['quantity'] for item in cart_items)
     cart_count = len(cart_items)
 
@@ -25,11 +40,10 @@ def checkout():
     """Страница оформления заказа."""
     db = get_db()
     cart_summary = db.execute(
-        'SELECT p.title, c.quantity, p.price*(1-p.discount/100.0) AS subtotal'
+        'SELECT p.id,p.title, c.quantity, p.price*(1-p.discount/100.0) AS subtotal'
         ' FROM cart c'
         ' JOIN product p ON p.id = c.product_id'
     ).fetchall()
-
     total = sum(item['subtotal'] * item['quantity'] for item in cart_summary)
     cart_count = len(cart_summary)
 
