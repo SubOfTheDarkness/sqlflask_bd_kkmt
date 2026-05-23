3ccb26e441cc54b75bb8b9c3dcfdd0b7be0d68e6
Octotacle <volosenukdaniil9@gmail.com>
2026-05-23 11:07:31 +0300

    великий фикс

--- app/__init__.py
+++ app/__init__.py
@@ -37,9 +37,9 @@ def create_app(test_config=None):
     app.register_blueprint(catalogue.bp)
     @app.errorhandler(404)
     def not_found(error):
-        return '<h1>Ошибка 404</h1><p><a href="/">Вернуться на глваную страницу</a></p>', 404
+        return '<h1>Ошибка 404</h1><p><a href="/">Вернуться на главную страницу</a></p>', 404
     
     @app.errorhandler(500)
     def server_error(error):
-        return '<h1>Ошибка 500(ошибка сервера)</h1><p><a href="/">Вернуться на глваную страницу</a></p>', 500
+        return '<h1>Ошибка 500(ошибка сервера)</h1><p><a href="/">Вернуться на главную страницу</a></p>', 500
     return app
